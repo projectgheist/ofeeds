e425f38b8fb5397f9c11feda7db1345c1c86a993
projectgheist <projectgheist@gmail.com>
2014-11-08 17:14:48 +0000

    [] modified config file for better appfog integration

--- config.js
+++ config.js
@@ -2,7 +2,11 @@
  */
 module.exports.Url = function() {
 	if (process.env.OPENSHIFT_APP_DNS) {
-		return 'http://' + process.env.OPENSHIFT_APP_DNS;
+        // Openshift
+        return 'http://' + process.env.OPENSHIFT_APP_DNS;
+	} else if (process.env.AF_APP_URL) {
+        // Appfog
+		return process.env.AF_APP_URL;
 	}
 	return 'http://localhost:' + exports.Port();
 };
@@ -11,7 +15,8 @@ module.exports.Url = function() {
  */
 module.exports.Dir = function() {
 	if (process.env.OPENSHIFT_REPO_DIR) {
-		return process.env.OPENSHIFT_REPO_DIR;
+        // Openshift
+        return process.env.OPENSHIFT_REPO_DIR;
 	}
 	return __dirname + '/';
 };
@@ -20,6 +25,7 @@ module.exports.Dir = function() {
  */
 module.exports.IpAddr = function() {
 	if (process.env.OPENSHIFT_NODEJS_IP) {
+        // Openshift
 		return process.env.OPENSHIFT_NODEJS_IP;
 	}
 	return '127.0.0.1';
@@ -56,14 +62,22 @@ module.exports.site = {
 /** Mongo DB Credentials
  */
 module.exports.db = function() {
+	// default database name
+	var defaultDbName = 'storageDB';
+    // Appfog
 	if (process.env.VCAP_SERVICES) {
 		var env = JSON.parse(process.env.VCAP_SERVICES);
-		return env['mongodb2-2.4.8'][0]['credentials'];
+        // support multiple mongodb configurations
+        if (env['mongodb-1.8']) {
+            return env['mongodb-1.8'][0]['credentials']; 
+        } else if (env['mongodb2-2.4.8']) {
+            return env['mongodb2-2.4.8'][0]['credentials'];
+        }
 	} else {
 		return {
 			hostname: 	process.env.OPENSHIFT_MONGODB_DB_HOST || 'localhost',
 			port: 		process.env.OPENSHIFT_MONGODB_DB_PORT || '27017',
-			dbname: 	process.env.OPENSHIFT_APP_NAME || 'storageDB',
+			dbname: 	process.env.OPENSHIFT_APP_NAME || defaultDbName,
 			username: 	process.env.OPENSHIFT_MONGODB_DB_USERNAME || '',
 			password: 	process.env.OPENSHIFT_MONGODB_DB_PASSWORD || ''
 		};
