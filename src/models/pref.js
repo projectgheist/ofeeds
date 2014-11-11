bcc37f12f8ad4697a6a8ac0bbeda8b6f8cc5ac14
projectgheist <projectgheist@gmail.com>
2014-11-11 01:05:32 +0000

    [+] add preferences model

---
+++ src/models/pref.js
@@ -0,0 +1,11 @@
+var mg = require('mongoose'),
+	ut = require('../utils');
+
+var Preference = mg.Schema({
+    user: 	ut.ref('User'),
+    stream: String,
+    key: 	String,
+    value: 	String
+});
+
+module.exports = mg.model('Pref', Preference);
